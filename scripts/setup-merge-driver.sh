7379cb1c9055f22745e4ee9784729876405334c8
Adam Spiers <adam@hypercerts.org>
2026-01-22 11:10:47 +1300

    fix: fix merge.schemas.driver
    Although it turns out this doesn't work with rebases anyway.

--- scripts/setup-merge-driver.sh
+++ scripts/setup-merge-driver.sh
@@ -4,6 +4,6 @@
 # When SCHEMAS.md has conflicts, it will be regenerated from lexicon files.
 # This script should be auto-run on npm install via the "prepare" script.
 
-git config --local merge.schemas.driver "npm run gen-schemas && git add -u -- %A"
+git config --local merge.schemas.driver "npm run gen-schemas-md && git add -u -- %A"
 
 echo "✅ Configured merge driver for SCHEMAS.md"
